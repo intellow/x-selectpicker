cce8c6c6d2b7296f2f0367ccbe66b5af06067699
Intellow <40676515+intellow@users.noreply.github.com>
2019-12-10 12:32:28 -0600

    Add search functionality

--- x-selectpicker.js
+++ x-selectpicker.js
@@ -8,14 +8,17 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 
-function xSelectpicker(selected, placeholder) {
+function xSelectpicker(selected, placeholder, searchEnabled) {
     return {
         selected: selected,
         showOptions: false,
         placeholder: placeholder,
+        searchEnabled: searchEnabled,
+        search: '',
         newSelection(name, value) {
             this.placeholder = name;
             this.selected = value;
+            this.showOptions = false;
         }
     }
 }
@@ -31,8 +34,12 @@ function createXselectpicker(select) {
         placeholder = "'" + select.getAttribute('placeholder') + "'";
     }
 
+    let searchEnabled = false;
+    if(select.getAttribute('search')) {
+        searchEnabled = true;
+    }
     // add a container to hold everything
-    select.insertAdjacentHTML('beforebegin', '<div id="'+ select.id +'_x_selectpicker_container" x-data="xSelectpicker('+select.value+', '+placeholder+')"></div>')
+    select.insertAdjacentHTML('beforebegin', '<div id="'+ select.id +'_x_selectpicker_container" x-data="xSelectpicker('+select.value+', '+placeholder+', '+searchEnabled+')"></div>');
     let parent = document.getElementById(select.id +'_x_selectpicker_container');
     // assign the classes on the select to the new parent
     let selectClasses = select.classList;
@@ -52,12 +59,58 @@ function createXselectpicker(select) {
     // create div to show selections
     parent.insertAdjacentHTML('beforeend', '<div id="'+ select.id + '_selection" x-on:click="showOptions = true" class="'+select.getAttribute('result-class')+'" x-text="placeholder"></div>');
     // create div to show options
-    parent.insertAdjacentHTML('beforeend', '<div id="'+ select.id + '_options" x-show="showOptions" x-on:click.away="showOptions = false" x-on:click="showOptions = false" class="'+select.getAttribute('dropdown-class')+'"></div>');
+    parent.insertAdjacentHTML('beforeend', '<div id="'+ select.id + '_options" x-show="showOptions" x-on:click.away="showOptions = false" class="'+select.getAttribute('dropdown-class')+'"></div>');
 
     let options = document.getElementById(select.id + '_options');
 
     let selectOptions = select.options;
-    Array.prototype.forEach.call(selectOptions, function(element, i) {
-        options.insertAdjacentHTML('beforeend', '<div id="' + select.id + '_option_' + element.value + '" class="'+select.getAttribute('option-class')+'" x-on:click="newSelection(\''+element.text+'\','+element.value+')">' + element.text + '</div>');
+
+    addOptions(select.id, '');
+
+    if (searchEnabled) {
+        options.insertAdjacentHTML('afterbegin', '<div id="'+select.id+'_search"><input id="'+select.id+'_search_input" type="text" x-model="search" x-on:keyup="updateOptionsTest(search, \''+select.id+'\')" class="'+select.getAttribute('search-class')+'"/></div>');
+    }
+}
+
+function updateOptionsTest(value, selectId)
+{
+    hideOptions(selectId, value);
+    focusOnSearchInput(selectId);
+}
+
+function hideOptions(selectId, searchText)
+{
+    let selectpickerOptions = document.getElementById(selectId+'_options');
+    console.log(selectpickerOptions.childNodes);
+    Array.prototype.forEach.call(selectpickerOptions.childNodes, function(element, i) {
+        console.log(element.id);
+        if(element.innerText.toLowerCase().includes(searchText.toLowerCase()) || element.id.includes('_search')) {
+            element.removeAttribute('style');
+        } else {
+            element.style.display = 'none';
+        }
     });
+}
+
+function focusOnSearchInput(selectId)
+{
+    document.getElementById(selectId+'_search_input').focus();
+}
+
+function addOptions(selectId, searchText)
+{
+    let originalSelect = document.getElementById(selectId);
+    let selectOptions = originalSelect.options;
+    let options = document.getElementById(selectId + '_options');
+    if(searchText.length > 0) {
+        Array.prototype.forEach.call(selectOptions, function(element, i) {
+            if(element.text.toLowerCase().includes(searchText.toLowerCase())) {
+                options.insertAdjacentHTML('beforeend', '<div id="' + originalSelect.id + '_option_' + element.value + '" class="'+originalSelect.getAttribute('option-class')+'" x-on:click="newSelection(\''+element.text+'\','+element.value+')">' + element.text + '</div>');
+            }
+        });
+    } else {
+        Array.prototype.forEach.call(selectOptions, function(element, i) {
+            options.insertAdjacentHTML('beforeend', '<div id="' + originalSelect.id + '_option_' + element.value + '" class="'+originalSelect.getAttribute('option-class')+'" x-on:click="newSelection(\''+element.text+'\','+element.value+')">' + element.text + '</div>');
+        });
+    }
 }
